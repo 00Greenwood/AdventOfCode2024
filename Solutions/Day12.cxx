8cc721afcf22ac3c07c421cfd7f2d80e30793f0b
00Greenwood <marcus_greenwood@hotmail.co.uk>
2024-12-12 18:45:59 -0800

    Solve Day 12 Part 2

--- Solutions/Day12.cxx
+++ Solutions/Day12.cxx
@@ -4,11 +4,18 @@
 
 #include <set>
 #include <map>
+#include <queue>
 
 namespace {
-struct Node {
+
+struct Group {
+  std::set<Position> positions;
+};
+
+struct Plot {
   char id;
-  std::set<Position> adjacent;
+  std::shared_ptr<Group> group;
+  std::map<Direction, Position> adjacent;
 };
 }
 
@@ -23,55 +30,103 @@ void Day12::initialize() {
 }
 
 std::string Day12::solve(Part part) const {
-  std::map<Position, Node> nodes;
+  std::map<Position, Plot> plots;
   for (int y = 0; y < m_input.size(); ++y) {
     for (int x = 0; x < m_input.at(y).size(); ++x) {
-      nodes[Position(x, y)] = Node(m_input[y][x]);
+      plots[Position(x, y)] = Plot(m_input[y][x]);
     }
   }
 
-  std::list<Position> positions;
-  for (const auto& [position, node] : nodes) {
-    positions.push_back(position);
-    for (const auto& direction : DIRECTIONS) {
-      Position adjacent = position + direction;
-      if (nodes.count(adjacent) > 0 && nodes.at(adjacent).id == node.id) {
-          nodes.at(position).adjacent.insert(adjacent);
-          nodes.at(adjacent).adjacent.insert(position);
+  std::vector<std::shared_ptr<Group>> groups;
+
+  for (const auto& [position, plot] : plots) {
+    if (plot.group != nullptr) {
+      continue;
+    }
+
+    std::queue<Position> positions;
+    positions.push(position);
+
+    while (positions.size() > 0) {
+      Position position = positions.front();
+      positions.pop();
+      if (plots.at(position).group != nullptr) {
+        continue;
       }
-    }
-  }
 
-  std::vector<std::set<Position>> groups;
-  while (positions.size() > 0) {
-    std::set<Position> group;
-    group.insert(positions.front());
-    std::list<Position> toCheck;
-    toCheck.push_back(positions.front());
-    while (toCheck.size() > 0) {
-      Position position = toCheck.front();
-      toCheck.pop_front();
-      for (const auto& adjacent : nodes.at(position).adjacent) {
-        if (group.contains(adjacent)) {
-          continue;
+      for (const auto& direction : DIRECTIONS) {
+        Position adjacent = position + direction;
+        if (plots.count(adjacent) > 0 && plots.at(adjacent).id == plots.at(position).id) {
+          positions.push(adjacent);
+          if (plots.at(adjacent).group != nullptr && plots.at(position).group == nullptr) {
+            plots.at(position).group = plots.at(adjacent).group;
+            plots.at(adjacent).group->positions.insert(position);
+          }
+          plots.at(position).adjacent[REVERSE_DIRECTIONS[direction]] = adjacent;
         }
-        group.insert(adjacent);
-        toCheck.push_back(adjacent);
+      }
+
+      if (plots.at(position).group == nullptr) {
+        std::set<Position> positions;
+        positions.insert(position);
+        groups.push_back(std::make_shared<Group>(positions));
+        plots.at(position).group = groups.back();
       }
     }
-    groups.push_back(group);
-    for (const auto& position : group) {
-      positions.remove(position);
-    }
   }
+
+  const std::vector<std::pair<Direction, Direction>> L = {{UP, RIGHT}, {RIGHT, DOWN}, {DOWN, LEFT}, {LEFT, UP}};
+  auto isInternalCorner = [&plots](const Position& position, const std::map<Direction, Position>& adjacent,
+                                   Direction first, Direction second) -> bool {
+    if (adjacent.contains(first) && adjacent.contains(second)) {
+      Position corner = position;
+      corner += adjacent.at(first) - position;
+      corner += adjacent.at(second) - position;
+      if (corner == position) {
+        return false;
+      }
+      if (plots.at(corner).id != plots.at(position).id) {
+        return true;
+      }
+    }
+    return false;
+  };
 
   size_t total = 0;
   for (const auto& group : groups) {
     size_t perimeter = 0;
-    for (const auto& position : group) {
-      perimeter += 4 - nodes.at(position).adjacent.size();
+    for (const auto& position : group->positions) {
+      if (part == PART_1) {
+        perimeter += 4 - plots.at(position).adjacent.size();
+
+      } else {
+        const auto& adjacent = plots.at(position).adjacent;
+        switch (adjacent.size()) {
+        case 0:
+          perimeter += 4;
+          break;
+        case 1:
+          perimeter += 2;
+          break;
+        case 2:
+          if ((adjacent.contains(UP) && adjacent.contains(DOWN)) ||
+              (adjacent.contains(LEFT) && adjacent.contains(RIGHT))) {
+            // No corners!
+          } else {
+            perimeter += 1;
+          }
+        case 3:
+        case 4:
+          for (const auto& [first, second] : L) {
+            if (isInternalCorner(position, adjacent, first, second)) {
+              perimeter += 1;
+            }
+          }
+          break;
+        }
+      }
     }
-    total += group.size() * perimeter;
+    total += group->positions.size() * perimeter;
   }
 
   return std::to_string(total);
--- Utilities/Direction.hxx
+++ Utilities/Direction.hxx
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <Position.hxx>
+#include <map>
 
 enum Direction { UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3 };
 
-static Position DIRECTIONS[4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+static Position DIRECTIONS[4] = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
+
+static std::map<Position, Direction> REVERSE_DIRECTIONS = {
+    {{0, -1}, UP}, {{1, 0}, RIGHT}, {{0, 1}, DOWN}, {{-1, 0}, LEFT}};
